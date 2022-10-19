6f03db5b9f72ef2b6aad99bd93927b67bff62db9
Utkarsh Vishnoi <78460632+utkarshvishnoii@users.noreply.github.com>
2022-10-19 11:33:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import yaml
+
+app = Flask(__name__)
+
+# Configure db
+#db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '*******'
+app.config['MYSQL_DB'] = 'flaskdb'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        # update it in db
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM users")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html',userDetails=userDetails)
+
+if __name__ == '__main__':
+    app.run(debug=True)
